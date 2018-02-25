a4aa67d87c0c176c0aa97041d3e68e81542df26e
Alberto Peña Abril <plagelao@gmail.com>
2018-02-25 19:39:11 +0000

    Basic beta version for npcs, getting the info from contentful

--- config.rb
+++ config.rb
@@ -1,8 +1,22 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+File.read('.env').lines do |line|
+  name, value = line.split('=')
+  ENV[name] = value
+end
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
+end
+
+activate :contentful do |f|
+  f.space         = {rpg: ENV['SPACE']}
+  f.access_token  = ENV['SECRET_TOKEN']
+  f.cda_query     = {limit: 100}
+  f.content_types = {
+    campaign: 'campaign'
+  }
 end
 
 # Layouts
@@ -26,6 +40,16 @@ page '/*.txt', layout: false
 #     which_fake_page: 'Rendering a fake page with a local variable'
 #   },
 # )
+
+if data.respond_to?('rpg')
+  data.rpg.campaign.each do |id, campaign|
+    # Assumes the file source/about/template.html.erb exists
+    proxy "/#{campaign.slug}.html", "/campaign/template.html", locals: { campaign: campaign }
+    campaign.npcs.each do |npc|
+      proxy "/#{campaign.slug}/#{npc.slug}.html", "/npc/template.html", locals: { npc: npc }
+    end
+  end
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
